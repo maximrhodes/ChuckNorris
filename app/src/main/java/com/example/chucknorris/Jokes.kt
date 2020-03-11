62a0214572aeb09e000cd1197c4cc0fbbc71c996
Azenaroth <maxim.rhodes@edu.esiee.fr>
2020-03-11 14:42:48 +0100

    I.1 List of static jokes

---
+++ app/src/main/java/com/example/chucknorris/Jokes.kt
@@ -0,0 +1,16 @@
+package com.example.chucknorris
+
+object Jokes{
+    val jokes:List<String> = listOf<String>(
+        "Peace disturbs Chuck Norris.",
+        "Iron Maiden wrote the song 'Run to the hills' after their brief encounter with Chuck Norris.",
+        "The real reason dinosaurs are extenct is because Chuck Norris went back in time.",
+        "When Medusa stared at Chuck Norris SHE turned to stone.",
+        "Chuck Norris has alredy been to Mars. This is why there is no signs of life there",
+        "Chuck Norris's shadow weighs 250 pounds and can kick your ass .",
+        "Chuck Norris's wife had a baby,he did all the breast feeding!",
+        "Chuck Norris doesn't wash his clothes. He disembowels them.",
+        "There's a word for someone who doesn't like Chuck Norris movies: pussy.",
+        "Chuck Norris knows the last digit of PI."
+    )
+}
