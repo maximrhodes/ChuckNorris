f5a4f404ec00dc9069fa57f671dbdc3b5315c57c
Azenaroth <maxim.rhodes@edu.esiee.fr>
2020-03-11 21:02:31 +0100

    I.3 Custom Adapter

---
+++ app/src/main/java/com/example/chucknorris/JokeAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.chucknorris
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class JokeAdapter  : RecyclerView.Adapter<JokeAdapter.JokeViewHolder>(){
+
+    var jokeList: List<String> = Jokes.jokes
+        set(newJokeList)
+        {
+            field = newJokeList
+            notifyDataSetChanged()
+        }
+
+    class JokeViewHolder(val joke: TextView) : RecyclerView.ViewHolder(joke)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
+        val textView = LayoutInflater.from(parent.context).inflate(R.layout.layout_view, parent, false) as TextView
+        return JokeViewHolder(textView)
+    }
+
+    override fun getItemCount(): Int {
+        return jokeList.size
+    }
+
+    override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
+        holder.joke.text = jokeList[position]
+    }
+}
